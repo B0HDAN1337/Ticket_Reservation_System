5a39792e0e56157ec6c8d033e93d78b74fcd4911
ghost <machuliak1337@gmail.com>
2025-03-07 19:41:35 +0100

    changed Birth Date to input without time

--- System Rezerwacji Biletów/Models/User.cs
+++ System Rezerwacji Biletów/Models/User.cs
@@ -5,7 +5,7 @@
         public int UserID { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
-        public DateTime BirthDate { get; set; }
+        public DateOnly BirthDate { get; set; }
         public string email { get; set; }
         public ICollection<Reservation> Reservations { get; set; }
     }
