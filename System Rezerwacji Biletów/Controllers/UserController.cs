57b16237695ab79e3accd1fe234e43bad9e206ae
ghost <machuliak1337@gmail.com>
2025-03-13 20:52:55 +0100

    Added function to save new user to database

--- System Rezerwacji Biletów/Controllers/UserController.cs
+++ System Rezerwacji Biletów/Controllers/UserController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 using System_Rezerwacji_Biletów.Data;
 using System_Rezerwacji_Biletów.Models;
 
@@ -10,31 +12,68 @@ namespace System_Rezerwacji_Biletów.Controllers
 
         public UserController(SystemReservationContext context)
         {
-            _context = context;
+            _context = context ?? throw new ArgumentNullException(nameof(context));
         }
+
 
         public IActionResult Create()
         {
             return View();
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> ListUser()
+        {
+            var users = await _context.Users.ToListAsync();
+            return View(users);
+        }
+
+        public IActionResult Edit()
         {
             return View();
         }
+
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("UserID, Name, LastName, BirthDate, email")] User user)
         {
-            if(ModelState.IsValid)
+            
+            if (ModelState.IsValid)
             {
                 _context.Users.Add(user);
                 await _context.SaveChangesAsync();
-
-                return View(nameof(Index));
+            
+                return RedirectToAction(nameof(ListUser));
             }
+
             return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, User user)
+        {
+            if (id != user.UserID)
+            {
+                return NotFound();  
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(ListUser));
+            }
+            else
+            {
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    Console.WriteLine($"Error: {error.ErrorMessage}");
+                }
+                return View(user);
+            }
+                return View(user);
         }
     }
 }
