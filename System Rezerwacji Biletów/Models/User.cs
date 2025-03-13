23f3b17fa41fbcaabfd1ac3565b1fe0338567869
ghost <machuliak1337@gmail.com>
2025-03-13 20:54:58 +0100

    Changed that 'Reservation' is not required during creating user

--- System Rezerwacji Biletów/Models/User.cs
+++ System Rezerwacji Biletów/Models/User.cs
@@ -1,12 +1,18 @@
-﻿namespace System_Rezerwacji_Biletów.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace System_Rezerwacji_Biletów.Models
 {
     public class User
     {
         public int UserID { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
-        public DateOnly BirthDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
         public string email { get; set; }
-        public ICollection<Reservation> Reservations { get; set; }
+        public ICollection<Reservation>? Reservations { get; set; }
+
+        
     }
 }
