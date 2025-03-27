d07b4d35cc2ea5d84adbf69cee3dca6f602514d4
B0HDAN1337 <148467696+B0HDAN1337@users.noreply.github.com>
2025-03-27 22:30:04 +0100

    Lab3 (#2)
    * added user and event controllers using with Repository method
    
    * Removed async fun

---
+++ System Rezerwacji Biletów/Repository/IEventRepository.cs
@@ -0,0 +1,14 @@
+﻿using System_Rezerwacji_Biletów.Models;
+
+namespace System_Rezerwacji_Biletów.Repository
+{
+    public interface IEventRepository
+    {
+        IQueryable<Event> GetAll();
+        Event GetById(int id);
+        Event Create(Event event_);
+        Task<Event> Update(int id, Event event_);
+        Event Delete(int id);
+        
+    }
+}
---
+++ System Rezerwacji Biletów/Repository/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿using System_Rezerwacji_Biletów.Data;
+using System_Rezerwacji_Biletów.Models;
+
+namespace System_Rezerwacji_Biletów.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly SystemReservationContext _context;
+
+        public UserRepository(SystemReservationContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            return _context.Users.AsQueryable();
+        }
+        public User GetById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        public User Create(User users)
+        {
+            _context.Users.Add(users);
+            _context.SaveChanges();
+            return users;
+        }
+
+        public async Task<User> Update(int id, User users)
+        {
+            var existingUser = await _context.Users.FindAsync(id);
+
+            existingUser.Name = users.Name;
+            existingUser.LastName = users.LastName;
+            existingUser.BirthDate = users.BirthDate;
+            existingUser.email = users.email;
+
+            await _context.SaveChangesAsync();
+            return existingUser;
+        }
+
+        public User Delete(int id)
+        {
+            var userModel = _context.Users.Find(id);
+
+            _context.Users.Remove(userModel);
+            _context.SaveChanges();
+            return userModel;
+        }
+
+    }
+}
