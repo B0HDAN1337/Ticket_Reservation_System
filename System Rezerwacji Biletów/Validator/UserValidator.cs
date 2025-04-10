2b37c393091c7a8bdae7903f390d38f989280498
ghost <machuliak1337@gmail.com>
2025-04-10 19:53:42 +0200

    Add validation rules

---
+++ System Rezerwacji Biletów/Validator/UserValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System_Rezerwacji_Biletów.Models;
+using System_Rezerwacji_Biletów.ViewModels;
+
+namespace System_Rezerwacji_Biletów.Validator
+{
+    public class UserValidator : AbstractValidator<UserViewModel>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Enter your name.");
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("Enter your last name.");
+            RuleFor(x => x.BirthDate).LessThan(x => DateTime.Now).WithMessage("The date of birth should not be today or any future date.");
+            RuleFor(x => x.email).EmailAddress().WithMessage("The email address must contain special characters.");
+        }
+    }
+}
